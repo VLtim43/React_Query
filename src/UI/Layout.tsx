5c96262a460355ddae888e2361ca266f911aeb7c
VLtim43 <fernandoreal.valentim@gmail.com>
2025-05-28 21:35:15 -0300

    update

--- src/UI/Layout.tsx
+++ src/UI/Layout.tsx
@@ -6,8 +6,8 @@ type Props = {
 
 export const Layout = ({ children }: Props) => {
   return (
-    <div className="min-h-screen flex flex-col bg-sepia-500 text-sepia-900 font-serif">
-      <header className="bg-sepia-600 border-b border-sepia-800 shadow p-4">
+    <div className="min-h-screen flex flex-col bg-sepia-400 text-sepia-900 font-serif">
+      <header className="bg-sepia-500 border-b border-sepia-800 shadow p-4">
         <h1 className="text-3xl font-bold text-sepia-900">
           📚 Mirage Bookstore
         </h1>
@@ -15,7 +15,7 @@ export const Layout = ({ children }: Props) => {
 
       <main className="flex-1 max-w-4xl mx-auto w-full p-4">{children}</main>
 
-      <footer className="bg-sepia-600 p-4 text-center text-sm text-sepia-900 border-t border-sepia-800">
+      <footer className="bg-sepia-500 p-4 text-center text-sm  border-sepia-800">
         Fake Amazon for Books — sepia edition
       </footer>
     </div>
